0a4263d26d639f21b3334a139e81e448c18a057d
HannahChristmas <hc109909@gmail.com>
2022-08-31 12:52:05 -0400

    Dropdown Event
    Event listener added to dropdown menu:
    
    When a house is selected, a list of characters from that house is populated.

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ fetch('http://hp-api.herokuapp.com/api/characters')
 
 let allCharacters = [];
 
+const dropdown = document.getElementById('house-dropdown');
+dropdown.addEventListener('change', houseChangeEventHandler);
+
+const ulArea = document.querySelector('ul');
+
+function houseChangeEventHandler(e) {
+    ulArea.innerHTML = '';
+    let selectedHouse = e.target.value;
+    console.log(e)
+
+    allCharacters.forEach(character => {
+        if(character.house === selectedHouse){
+            const ul = document.createElement('ul')
+
+            ul.textContent = character.name
+            ulArea.appendChild(ul)
+        }
+    })
+}
