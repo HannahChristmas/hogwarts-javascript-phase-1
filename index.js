55393705011103c58c2cee8d88232a2a8e5a7fca
HannahChristmas <hc109909@gmail.com>
2022-08-31 13:28:51 -0400

    Mouseover Purple
    Event listener added to character ul- character changes to purple upon mouseover.

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ const ulArea = document.querySelector('ul');
 function houseChangeEventHandler(e) {
     ulArea.innerHTML = '';
     let selectedHouse = e.target.value;
-    console.log(e)
 
     allCharacters.forEach(character => {
         if(character.house === selectedHouse){
             const ul = document.createElement('ul')
+                ul.addEventListener('mouseover', (e) => {
+                    e.target.style.color = 'purple';
+                })
 
             ul.textContent = character.name
             ulArea.appendChild(ul)
