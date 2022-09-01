fafb6e77d7a2013313f693e4a88a27d3e0b17f28
HannahChristmas <hc109909@gmail.com>
2022-09-01 10:11:20 -0400

    Click to add character card
    Upon clicking a character's name, that character's photo and house will appear on the DOM

--- index.js
+++ index.js
@@ -28,9 +28,28 @@ function houseChangeEventHandler(e) {
                 ul.addEventListener('click', (e) => {
                     e.target.style.color = 'green';
                 })
+                ul.addEventListener('click', renderCharacterCard)
 
             ul.textContent = character.name
             ulArea.appendChild(ul)
         }
     })
+}
+
+const character = []
+let ulCharacter = document.querySelector('.character-container')
+
+function renderCharacterCard(e) { 
+    let character = allCharacters.filter(character => character.name === e.target.outerText)[0];
+
+    const ul = document.createElement('ul')
+    const img = document.createElement('img')
+    const h3 = document.createElement('h3')
+   
+    img.src = `${character.image}`
+    h3.textContent = `House: ${character.house}`
+
+    ul.append(img, h3)
+    document.querySelector('.float-container').append(ul)
+    allCharacters.push(ul);
 }
